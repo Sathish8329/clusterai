8a49c63f62acb4e689cee313c7765b88a4f1cf9c
Sathish8329 <131761544+Sathish8329@users.noreply.github.com>
2023-11-23 15:44:27 +0530

    Update handler# vim login.controller.js

--- authh/auth/src/routes/login/handler# vim login.controller.js
+++ authh/auth/src/routes/login/handler# vim login.controller.js
@@ -1,58 +1,58 @@
-          subpermissions: [...new Set(e.subpermissions)],
-        };
-      });
+import { executeErrorLog } from "../../../error-logger/errorLogger";
+import { generetaesalthash, newConnection } from "../../../utils";
+import CryptoJS from "crypto-js";
+var jwt = require("jsonwebtoken");
+const { Worker } = require("worker_threads");
 
-      let token = jwt.sign(
-        {
-          email: docs?.[0]?.email,
-          user_id: docs?.[0]?._id,
-          client_id: docs?.[0]?.client_id,
-          entity_id: docs?.[0]?.entity_id,
-          slug: slugdata,
-          timeStamp: new Date().toISOString(),
-        },
-        process.env.JWT_SECRET
-      );
-
-      resolve({
-        statusCode: 200,
-        type: "Success",
-        message: "Authenticated",
-        data: {
-          token,
-          ...{
-            email: docs?.[0]?.email,
-            img_url: docs?.[0]?.img_url,
-            user_id: docs?.[0]?._id,
-            role_data: "",
-            reporting_to: docs?.[0]?.reporting_to,
-            role: LogedUserRole,
-            name: docs?.[0]?.name,
-            entity_id: docs?.[0]?.entity_id,
-            client: clientDetail,
-            permissiondata: structuredValueresult,
-            customrole: LogedUserRole,
-            employeeid: docs?.[0]?.emp_id,
-            employeelocation: docs?.[0]?.employeelocation,
-            gender: docs?.[0]?.gender,
-            managerid: managerdata?.emp_id,
-            user_phone: docs?.[0]?.phone,
-            country_code: docs?.[0]?.country_code,
-            managername: managerdata?.name,
-            manageremail: managerdata?.email,
-            managerphone: managerdata?.phone,
-            // subpermission: structuredSub,
-          },
-        },
-      });
-    } catch (err) {
-      executeErrorLog(request, err);
-      reject({
-        statusCode: 500,
-        message: "Internal Server Error, Try agian later!",
-        err,
-      });
-    }
-  });
+export const login = async (request, reply, fastify) => {
+  try {
+    //Calling login function to check credential is valid or not
+    let output = await loginHander(request, request.headers, fastify);
+    //sending response
+    reply.code(200).send({
+      statusCode: output.statusCode,
+      type: output.type,
+      message: output.message,
+      data: { ...output.data },
+    });
+  } catch (err) {
+    executeErrorLog(request, err);
+    reply.code(500).send({
+      statusCode: 500,
+      type: "Error",
+      message: err.message,
+      ...err,
+    });
+    fastify.log.error(err);
+  }
 };
-                                                                                                                                                                                                462,8         Bot
+
+const loginValidation = (request, subDomain, custom_domain_name) => {
+        console.log(request?.headers?.referer)
+  return (
+    request?.headers &&
+    request?.headers?.referer &&
+    request?.headers?.referer != process.env.FRONT_END_URL + "/" &&
+    !(subDomain && request?.headers?.referer?.includes(subDomain)) &&
+    !(
+      custom_domain_name &&
+      request?.headers?.referer?.includes(custom_domain_name)
+    ) &&
+    !request?.headers?.referer?.includes("localhost") &&
+    !request?.headers?.referer?.includes("engage") &&
+          !request?.headers?.referer?.includes("http://35.154.125.90:3000/") &&
+    !request?.headers?.referer?.includes("http://172.16.0.157/")
+  );
+};
+
+export const loginHander = async (request, reply, fastify) => {
+  const { email, subDomain, mobile, countryCode, otp } = request.body;
+  let password = "";
+  if (email) {
+    password = CryptoJS.AES.decrypt(
+      request.body.password,
+      process.env.JWT_SECRET
+    ).toString(CryptoJS.enc.Utf8);
+  }
+  return new Promise(async (resolve, reject) => {
+                                                                                                                                                                                                9,8           Top
